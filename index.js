22e77695c2f07ad9543cc94524020edb93cd6a4d
Yash Jain <yashdm.jain@gmail.com>
2022-03-11 21:57:22 +0530

    Added the todo api

--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
-const users = []
-app.get('/getUsers', (req, res) => {
+let todos = []
+app.get('/getTodo', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.json(users)
+    console.log('test')
+    res.json(todos)
 })
 
-app.post('/addUser', (req, res) => {
+app.post('/addTodo', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    const user = req.body.userDetails;
-    users.push(user);
-    res.json("user addedd");
+    const todo = req.body.todoDetails;
+    todos.push(todo);
+    res.json("todo added");
 })
 
-app.listen(3000, () => {
+app.put('/updateTodo/:id', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    const newTodo = req.body.todoDetails;
+    todos = todos.map((todo) => {
+        if (todo.id === req.params.id) {
+            return newTodo
+        }
+        return todo;
+    });
+    
+    res.json(todos)
+})
+
+app.listen(process.env.port || 4000, () => {
     console.log('connected')
 })
